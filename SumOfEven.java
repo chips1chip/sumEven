dd10c97a4f5d84f01430061da304e0792a42fde4
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:37:28 +0530

    Update SumOfEven.java

--- SumOfEven.java
+++ SumOfEven.java
@@ -1,13 +1,13 @@
 public class SumOfEven {
     public static void main(String[] args) {
-        int sum=0;
-        int i=1;
-        while(i<=100){
-            if(i%2==0){
-                sum=sum+i;
+        int sum = 0;  // Variable to store the sum
+        int i = 1;  
+        while (i <= 100) {  // Loop through numbers from 1 to 100
+            if (i % 2 == 0) {  // Check if the number is even
+                sum = sum + i;  // Add even number to sum
             }
-            i++;
+            i++; 
         }
-        System.out.println(sum);
+        System.out.println(sum);  
     }
 }
